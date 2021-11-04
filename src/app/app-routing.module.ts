f5df813977a9dbedce8e3374a87ba30b449ea7ae
TomasTV <toralestomas@gmail.com>
2021-11-04 12:45:22 -0300

    routing finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PetsComponent } from './components/pets/pets.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PetsComponent },
+  { path: 'about', component: AboutPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
