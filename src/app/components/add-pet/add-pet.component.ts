eea9a57f54385e2ccdcf9d4453e0b11b99a47008
TomasTV <toralestomas@gmail.com>
2021-11-04 12:46:19 -0300

    add pet component done

---
+++ src/app/components/add-pet/add-pet.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pet',
+  templateUrl: './add-pet.component.html',
+  styleUrls: ['./add-pet.component.css'],
+})
+export class AddPetComponent implements OnInit {
+  @Output() onAddPet = new EventEmitter();
+
+  breed!: string;
+  url!: string;
+  description!: string;
+  favorite: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.breed != '' && this.description != '') {
+      const newPet = {
+        breed: this.breed,
+        url: this.url,
+        description: this.description,
+        favorite: this.favorite,
+      };
+
+      this.onAddPet.emit(newPet);
+      this.breed = '';
+      this.url = '';
+      this.description = '';
+    } else {
+      alert('Must enter a breed & description!');
+    }
+  }
+}
